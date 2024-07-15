ea0438b3d70f49af6b0e8aa3b44b2717c74d5a3e
afablelorenz <afablelorenz@gmail.com>
2024-07-15 12:44:23 +0200

    update

--- laravel-primi-passi/routes/web.php
+++ laravel-primi-passi/routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = [
+        'message' => 'Hello world!'
+    ];
+    return view('home',$data);
 });
